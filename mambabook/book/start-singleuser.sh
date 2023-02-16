c827f0f939e580c488f12ee95a517d90c4efe948
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2023-02-16 23:26:23 +0700

    feat(jupyterlab): add lsp, autocompletion

--- mambabook/book/start-singleuser.sh
+++ mambabook/book/start-singleuser.sh
@@ -6,8 +6,10 @@ set -e
 
 # set default ip to 0.0.0.0
 if [[ "${NOTEBOOK_ARGS} $*" != *"--ip="* ]]; then
-    NOTEBOOK_ARGS="--ip=0.0.0.0 ${NOTEBOOK_ARGS}"
+	NOTEBOOK_ARGS="--ip=0.0.0.0 ${NOTEBOOK_ARGS}"
 fi
+
+ln -s / .lsp_symlink # For jupyterlab_lsp plugin to jump to definition
 
 # shellcheck disable=SC1091,SC2086
 . /usr/local/bin/start.sh jupyterhub-singleuser ${NOTEBOOK_ARGS} "$@"
